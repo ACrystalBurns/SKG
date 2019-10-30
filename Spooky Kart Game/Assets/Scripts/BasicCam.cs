4f214e5577debbc14e4bfcb9b048df67320afa77
Flowerfoxified <43792086+Flowerfoxified@users.noreply.github.com>
2019-10-30 16:44:44 -0400

    made very basic kart controls
    A test scene was made with a moving kart. Still very much a work in progress.

---
+++ Spooky Kart Game/Assets/Scripts/BasicCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCam : MonoBehaviour
+{
+    public GameObject kart; //Stores reference to kart
+    private Vector3 offset; //Stores offset distance between kart and camera
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - kart.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //Sets camera position to be same as kart's, but offset by calculated offset distance
+        transform.position = kart.transform.position + offset;
+    }
+}
---
+++ Spooky Kart Game/Assets/Scripts/TestDrive.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDrive : MonoBehaviour
+{
+    public float speed = 5.0f; //Speed of kart
+    public float rotSpeed = 5.0f;  //Rotation speed of kart
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Make kart respond to key presses
+        if (Input.GetKey(KeyCode.W))    //^
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.S))   //v
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * -speed, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.A))   //<
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * -rotSpeed, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.D))   //>
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * rotSpeed, Space.Self);
+        }
+    }
+}
